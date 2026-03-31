3815c252cb0acf827c56a44cf24b4a9f1c715d23
Sergio Marcelino <sergio@filho.org>
2026-03-31 17:49:22 -0300

    chore: remove stream api

--- apps/website/src/hooks/use-todos.ts
+++ apps/website/src/hooks/use-todos.ts
@@ -1,5 +1,5 @@
-import { useStreamGroup } from '@motiadev/stream-client-react'
-import { useCallback, useMemo } from 'react'
+import type { StreamChangeEvent } from 'iii-browser-sdk/stream'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 import { iii } from '../lib/iii'
 
 export interface Todo {
@@ -9,10 +9,48 @@ export interface Todo {
 }
 
 export function useTodos() {
-  const { data: todos } = useStreamGroup<Todo>({
-    streamName: 'todo',
-    groupId: 'todos',
-  })
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  useEffect(() => {
+    void iii
+      .trigger<Record<string, never>, { items: Todo[] }>({
+        function_id: 'todos::list',
+        payload: {},
+      })
+      .then(({ items }) => setTodos(items))
+
+    const funcRef = iii.registerFunction({ id: 'ui::on-todo-change' }, async (input: StreamChangeEvent) => {
+      const todo = input.event.data as Todo
+
+      console.log('on-todo-change', input)
+
+      switch (input.event.type) {
+        case 'create':
+          setTodos((prev) => [...prev, todo])
+          break
+        case 'update':
+          console.log('update-todo', input)
+          setTodos((prev) => prev.map((t) => (t.id === input.id ? todo : t)))
+          break
+        case 'delete':
+          setTodos((prev) => prev.filter((t) => t.id !== input.id))
+          break
+      }
+
+      return {}
+    })
+
+    const trigger = iii.registerTrigger({
+      type: 'stream',
+      function_id: funcRef.id,
+      config: { stream_name: 'todo', group_id: 'todos' },
+    })
+
+    return () => {
+      trigger.unregister()
+      funcRef.unregister()
+    }
+  }, [])
 
   const addTodo = useCallback(async (title: string) => {
     await iii.trigger({
--- apps/website/src/main.tsx
+++ apps/website/src/main.tsx
@@ -1,4 +1,3 @@
-import { MotiaStreamProvider } from '@motiadev/stream-client-react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -6,8 +5,6 @@ import App from './App'
 
 createRoot(document.getElementById('app')!).render(
   <StrictMode>
-    <MotiaStreamProvider address="ws://localhost:3112">
-      <App />
-    </MotiaStreamProvider>
+    <App />
   </StrictMode>,
 )
